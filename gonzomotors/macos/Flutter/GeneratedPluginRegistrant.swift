0c1a2e6ca35186a197b4f154966670879b9a5fe4
Khursandbek <khursand.khaitbaev@mail.ru>
2025-09-20 16:34:25 +0500

    Add flutter gen color and assets

--- gonzomotors/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gonzomotors/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,24 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
+import firebase_messaging
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
