67d28c0137dce89a4aded9035812dadf7d44786f
Khursandbek <khursand.khaitbaev@mail.ru>
2025-09-23 13:14:36 +0500

    Add colors gen file

---
+++ gonzomotors/lib/gen/colors.gen.dart
@@ -0,0 +1,79 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #2196F3
+  static const Color accentInfo = Color(0xFF2196F3);
+
+  /// Color: #D32F2F
+  static const Color accentPrimary = Color(0xFFD32F2F);
+
+  /// Color: #FFC107
+  static const Color accentSecondary = Color(0xFFFFC107);
+
+  /// Color: #4CAF50
+  static const Color accentSuccess = Color(0xFF4CAF50);
+
+  /// Color: #212121
+  static const Color backgroundInverse = Color(0xFF212121);
+
+  /// Color: #EEEEEE
+  static const Color backgroundMuted = Color(0xFFEEEEEE);
+
+  /// Color: #F5F5F5
+  static const Color backgroundPrimary = Color(0xFFF5F5F5);
+
+  /// Color: #FFFFFF
+  static const Color backgroundSecondary = Color(0xFFFFFFFF);
+
+  /// Color: #FFFFFF
+  static const Color contentInverse = Color(0xFFFFFFFF);
+
+  /// Color: #99757575
+  static const Color contentMuted = Color(0x99757575);
+
+  /// Color: #212121
+  static const Color contentPrimary = Color(0xFF212121);
+
+  /// Color: #757575
+  static const Color contentSecondary = Color(0xFF757575);
+
+  /// Color: #33757575
+  static const Color lineBorder = Color(0x33757575);
+
+  /// Color: #1F757575
+  static const Color lineDivider = Color(0x1F757575);
+
+  /// Color: #D32F2F
+  static const Color statusError = Color(0xFFD32F2F);
+
+  /// Color: #2196F3
+  static const Color statusInfo = Color(0xFF2196F3);
+
+  /// Color: #4CAF50
+  static const Color statusSuccess = Color(0xFF4CAF50);
+
+  /// Color: #FFC107
+  static const Color statusWarning = Color(0xFFFFC107);
+
+  /// Color: #212121
+  static const Color surfaceInverse = Color(0xFF212121);
+
+  /// Color: #FFFFFF
+  static const Color surfacePrimary = Color(0xFFFFFFFF);
+
+  /// Color: #F5F5F5
+  static const Color surfaceSecondary = Color(0xFFF5F5F5);
+}
